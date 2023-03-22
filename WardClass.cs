3c7a41adb02e5ccc1d73ff1eaf8f04d875200cb8
S00242123 <S00242123@A-9R9R273>
2023-03-22 10:10:08 +0000

    Add project files.

---
+++ WardClass.cs
@@ -0,0 +1,33 @@
+﻿using CA1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA1
+{
+    public class Ward
+    {
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+
+        public List<Patient> Patients { get; set; }
+
+        public Ward()
+        {
+            Name = string.Empty;
+            //Capacity = "";
+            Patients = new List<Patient>(); 
+        }
+    }
+
+
+    public class Patient
+    {
+        public string Name { get; set; }
+        public DateTime DOB { get; set; }
+        public string BloodType { get; set; }
+    }
+}
+
